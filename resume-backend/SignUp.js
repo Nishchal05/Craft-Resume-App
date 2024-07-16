bdaa1c0a2a5506fe09b0dcebed5479dc00d84c62
Nishchal05 <143197039+Nishchal05@users.noreply.github.com>
2024-07-17 00:51:39 +0530

    Update SignUp.js

--- resume-backend/SignUp.js
+++ resume-backend/SignUp.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose')
 const SignUpdata=new mongoose.Schema({
     name:String,
     email:String,
-    password:String
+    password:String,
+    createdAt: { type: Date, default: Date.now }
 })
 module.exports=mongoose.model('SignUpdata' , SignUpdata);
