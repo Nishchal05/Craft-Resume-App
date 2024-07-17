97cf00f5be6ecad95cdcdbc2d4816d61a11fd585
Nishchal05 <143197039+Nishchal05@users.noreply.github.com>
2024-07-17 22:28:35 +0530

    Update Navbar.jsx

--- resume-frontend/src/Resume-content/Navbar/Navbar.jsx
+++ resume-frontend/src/Resume-content/Navbar/Navbar.jsx
@@ -4,12 +4,16 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { MatcherContext } from '../../Login';
+import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
+  const navigate=useNavigate();
   const [mobileView, setMobileView] = useState(false);
   const { match } = useContext(MatcherContext); 
   const { setMatch } = useContext(MatcherContext);
   const logout=()=>{
     setMatch(false);
+  setMobileView(!mobileView)
+    navigate('/')
   }
   return (
     <header>
@@ -32,10 +36,10 @@ const Navbar = () => {
       {mobileView && (
         <div className='mobile-view-icon'>
           <ul className="mobile-links">
-            <li><Link to="/">Home</Link></li>
-            <li>Templates</li>
-            {match?<li onClick={logout}>Logout</li>:<div className='login-signup'><li><Link to="/Login">Login</Link></li>
-              <li><Link to="/SignUp">SignUp</Link></li></div>}
+            <li onClick={()=>{setMobileView(!mobileView)}}><Link to="/" >Home</Link></li>
+            <li  onClick={()=>{setMobileView(!mobileView)}}>Templates</li>
+            {match?<li onClick={logout}>Logout</li>:<div className='login-signup'id='login-signup'><li  onClick={()=>{setMobileView(!mobileView)}}><Link to="/Login">Login</Link></li>
+              <li  onClick={()=>{setMobileView(!mobileView)}}><Link to="/SignUp" >SignUp</Link></li></div>}
           </ul>
         </div>
       )}
