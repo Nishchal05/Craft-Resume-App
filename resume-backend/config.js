eb2f47a73d125bc185c4c86f6f0ab0d7d971ef83
Nishchal05 <143197039+Nishchal05@users.noreply.github.com>
2024-07-23 22:24:05 +0530

    Update config.js

--- resume-backend/config.js
+++ resume-backend/config.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1); // Exit process with failure
+    process.exit(1); 
   }
 };
 
